c7f04c69153624ba5e0bff336a778d6df55345e3
Brokkk <112681609+brokkken@users.noreply.github.com>
2022-11-23 11:50:52 +0400

    Add files via upload

--- sait-gotoviy/js/script.js
+++ sait-gotoviy/js/script.js
@@ -1,11 +1,16 @@
 $('.reviews-slider').slick({
     infinite: true,
+    centerMode: true,
     slidesToShow: 3,
     slidesToScroll: 1
-})
-sliderSecondBlock()
-function sliderSecondBlock() {
-    $('.reviews .reviewsBlock .info-block .item').css('opacity, 0.3');
-    $('.reviews .reviewsBlock .info-block .item.slick-active').eq(1).css('opacity', 1);
-}
+    });
+    sliderSecondBlock()
+    function sliderSecondBlock(){
+    $('.reviews .reviewsBlock .info-block .item.slick-active').eq(1).addClass('active')
+    }
+    $('button').on('click', function(){
+    $('.reviews .reviewsBlock .info-block .item.slick-active').removeClass('active')
+    $('.reviews .reviewsBlock .info-block .item.slick-center').addClass('active')
+    })
+    
    
